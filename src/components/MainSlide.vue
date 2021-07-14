25055097ad8d1db61060e1ac33d1d13f6b9d3a37
sergo93@gmail.com <sergo93@gmail.com>
2021-07-14 20:58:48 +0400

    start

---
+++ src/components/MainSlide.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main-slide container">
+    <div class="main-slide__inner">
+      <h1 class="main-slide__title">
+        <div class="main-slide__title-out">
+          <div class="main-slide__title-in">CORE</div>
+        </div>
+        <div class="main-slide__title-out">
+          <div class="main-slide__title-in">VISION</div>
+        </div>
+      </h1>
+      <div class="main-slide__description">
+        <p>Multi Chain PoS Network focused
+          on Interoperability, Scalability and Usability.
+          It utilizes AI Enabled Proof of Stake Algorithm, Multi Layered Consensus Model and Voting
+          Based Governance.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'MainSlide'
+}
+</script>
+<style scoped>
+  .main-slide{
+    height: 100%;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,50 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home-slider">
+    <hooper>
+      <slide>
+        <MainSlide/>
+      </slide>
+      <slide>
+        slide 2
+      </slide>
+      <slide>
+        slide 3
+      </slide>
+    </hooper>
+    <Game v-if="false"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Hooper, Slide } from 'hooper'
+import 'hooper/dist/hooper.css'
+import Game from '@/components/Game.vue'
+import MainSlide from '../components/MainSlide'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MainSlide,
+    Game,
+    Hooper,
+    Slide
   }
 }
 </script>
+<style>
+  .home-slider{
+    height: 100vh;
+    width: 100%;
+  }
+  .hooper-track{
+    /*transition-delay: 1s!important;*/
+    /*transition-duration: 1s!important;*/
+  }
+  .hooper-slide{
+    height: 100vh;
+  }
+  .hooper{
+    height: 100%;
+  }
+</style>

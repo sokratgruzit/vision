29ccc92416fadfac890e5c79832bbe8d9c6915fa
David Tavadze <tavadzed@gmail.com>
2021-07-26 18:22:30 +0400

    Added noise

--- src/components/MainSlide.vue
+++ src/components/MainSlide.vue
@@ -35,7 +35,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 
-
 export default {
   name: 'MainSlide',
   data () {
--- src/views/Roadmap.vue
+++ src/views/Roadmap.vue
@@ -6,6 +6,7 @@
 
 <script>
 import * as THREE from 'three';
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module.js';
 const TWEEN = require('@tweenjs/tween.js');
 
 export default {
@@ -51,6 +52,7 @@ export default {
 
       this.uniforms = {
         tex: { type: "t", value: texture },
+        time: { type: "f", value: 0.0 },
         resolution: { type: "v4", value: new THREE.Vector4() }
       };
 
@@ -74,10 +76,191 @@ export default {
           varying vec2 vUv;
           varying vec3 vPosition;
           uniform sampler2D tex;
+          varying float noise;
+          uniform float time;
+
+          vec3 mod289(vec3 x)
+          {
+            return x - floor(x * (1.0 / 289.0)) * 289.0;
+          }
+
+          vec4 mod289(vec4 x)
+          {
+            return x - floor(x * (1.0 / 289.0)) * 289.0;
+          }
+
+          vec4 permute(vec4 x)
+          {
+            return mod289(((x*34.0)+1.0)*x);
+          }
+
+          vec4 taylorInvSqrt(vec4 r)
+          {
+            return 1.79284291400159 - 0.85373472095314 * r;
+          }
+
+          vec3 fade(vec3 t) {
+            return t*t*t*(t*(t*6.0-15.0)+10.0);
+          }
+
+          // Classic Perlin noise
+          float cnoise(vec3 P)
+          {
+            vec3 Pi0 = floor(P); // Integer part for indexing
+            vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+            Pi0 = mod289(Pi0);
+            Pi1 = mod289(Pi1);
+            vec3 Pf0 = fract(P); // Fractional part for interpolation
+            vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+            vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+            vec4 iy = vec4(Pi0.yy, Pi1.yy);
+            vec4 iz0 = Pi0.zzzz;
+            vec4 iz1 = Pi1.zzzz;
+
+            vec4 ixy = permute(permute(ix) + iy);
+            vec4 ixy0 = permute(ixy + iz0);
+            vec4 ixy1 = permute(ixy + iz1);
+
+            vec4 gx0 = ixy0 * (1.0 / 7.0);
+            vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+            gx0 = fract(gx0);
+            vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+            vec4 sz0 = step(gz0, vec4(0.0));
+            gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+            gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+            vec4 gx1 = ixy1 * (1.0 / 7.0);
+            vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+            gx1 = fract(gx1);
+            vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+            vec4 sz1 = step(gz1, vec4(0.0));
+            gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+            gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+            vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+            vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+            vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+            vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+            vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+            vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+            vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+            vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+            vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+            g000 *= norm0.x;
+            g010 *= norm0.y;
+            g100 *= norm0.z;
+            g110 *= norm0.w;
+            vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+            g001 *= norm1.x;
+            g011 *= norm1.y;
+            g101 *= norm1.z;
+            g111 *= norm1.w;
+
+            float n000 = dot(g000, Pf0);
+            float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+            float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+            float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+            float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+            float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+            float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+            float n111 = dot(g111, Pf1);
+
+            vec3 fade_xyz = fade(Pf0);
+            vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+            vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+            float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+            return 2.2 * n_xyz;
+          }
+
+          // Classic Perlin noise, periodic variant
+          float pnoise(vec3 P, vec3 rep)
+          {
+            vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period
+            vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period
+            Pi0 = mod289(Pi0);
+            Pi1 = mod289(Pi1);
+            vec3 Pf0 = fract(P); // Fractional part for interpolation
+            vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+            vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+            vec4 iy = vec4(Pi0.yy, Pi1.yy);
+            vec4 iz0 = Pi0.zzzz;
+            vec4 iz1 = Pi1.zzzz;
+
+            vec4 ixy = permute(permute(ix) + iy);
+            vec4 ixy0 = permute(ixy + iz0);
+            vec4 ixy1 = permute(ixy + iz1);
+
+            vec4 gx0 = ixy0 * (1.0 / 7.0);
+            vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+            gx0 = fract(gx0);
+            vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+            vec4 sz0 = step(gz0, vec4(0.0));
+            gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+            gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+            vec4 gx1 = ixy1 * (1.0 / 7.0);
+            vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+            gx1 = fract(gx1);
+            vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+            vec4 sz1 = step(gz1, vec4(0.0));
+            gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+            gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+            vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+            vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+            vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+            vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+            vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+            vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+            vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+            vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+            vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+            g000 *= norm0.x;
+            g010 *= norm0.y;
+            g100 *= norm0.z;
+            g110 *= norm0.w;
+            vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+            g001 *= norm1.x;
+            g011 *= norm1.y;
+            g101 *= norm1.z;
+            g111 *= norm1.w;
+
+            float n000 = dot(g000, Pf0);
+            float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+            float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+            float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+            float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+            float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+            float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+            float n111 = dot(g111, Pf1);
+
+            vec3 fade_xyz = fade(Pf0);
+            vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+            vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+            float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+            return 2.2 * n_xyz;
+          }
+
+          float turbulence( vec3 p ) {
+              float w = 100.0;
+              float t = -.5;
+              for (float f = 1.0 ; f <= 10.0 ; f++ ){
+                  float power = pow( 2.0, f );
+                  t += abs( pnoise( vec3( power * p ), vec3( 10.0, 10.0, 10.0 ) ) / power );
+              }
+              return t;
+          }
 
           void main() {
             vUv = uv;
-            vec4 mvPosition = modelViewMatrix * vec4(position, 1.);
+
+            noise = 10.0 *  -.10 * turbulence( .5 * normal + time );
+            float b = 5.0 * pnoise( 0.05 * position + vec3( 2.0 * time ), vec3( 100.0 ) );
+            float displacement = - noise + b;
+            vec3 newPosition = position + normal * displacement;
+            vec4 mvPosition = modelViewMatrix * vec4(newPosition, 1.);
             gl_PointSize = 2.;
             gl_Position = projectionMatrix * mvPosition;
           }
@@ -87,20 +270,23 @@ export default {
           uniform vec4 resolution;
           varying vec2 vUv;
           varying vec3 vPosition;
+          varying float noise;
           
           void main() {
+            vec3 color = vec3( vUv * ( 1. - 2. * noise ), 0.0 );
             vec4 tt = texture2D(tex, vUv);
             gl_FragColor = vec4(vUv,0.,1.);
             gl_FragColor = tt;
           }
-        `
+        `,
+        morphTargets: true
       });
 
       var sLight = new THREE.SpotLight(0xffffff);
       sLight.position.set(-100, 100, 100);
       this.scene.add(sLight);
 
-      var aLight = new THREE.AmbientLight(0xffffff);
+      var aLight = new THREE.PointLight(0xffffff, 20);
       this.scene.add(aLight);
       
       this.roadmapMesh = new THREE.Points(this.roadmapGeo, this.roadmapMat);
@@ -120,12 +306,13 @@ export default {
     },
     animate: function () {
       let center = new THREE.Vector3(0,0,0);
-      const theTime = performance.now() * .001;
+      const theTime = performance.now() * 0.001;
       var pos = this.roadmapGeo.attributes.position;
       var vec3 = new THREE.Vector3(); 
       var magnitude = 5;
       var waveSize = 12;
       var rotationAngle = 0.07;
+      this.roadmapMat.uniforms.time.value = theTime / 10;
 
       if (this.isPointerDown) {
         this.camera.position.x += (this.mouseX * 4 - this.camera.position.x) * 0.005;
@@ -165,17 +352,6 @@ export default {
 
       pos.needsUpdate = true;
 
-      /*let iSin = 0;
-      for (let ix = 0, l = pos.count; ix < l; ix++) {
-        for (let iy = 0, h = pos.count; iy < h; iy++) {
-          pos[iSin + 1] = (Math.sin((ix + this.count) * 0.1) * 150) + (Math.sin((iy + this.count) * 0.3) * 150);
-
-          iSin += 3;
-        }
-      }
-      this.roadmapMesh.geometry.attributes.position.needsUpdate = true;
-      this.count += 0.1;*/
-
       requestAnimationFrame(this.animate);
       TWEEN.update();
       this.render();
@@ -183,6 +359,27 @@ export default {
     render: function () {
       this.renderer.setPixelRatio(window.devicePixelRatio);
       this.renderer.render(this.scene, this.camera);
+    },
+    wheelScroll: function(event) {
+      if (event.isPrimary === false) return;
+
+      this.mouseX = event.clientX - this.windowHalfX;
+
+      if (this.camera.position.x < -200) {
+        new TWEEN.Tween(this.camera.position)
+        .to({ x: -200 }, 3000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .start();
+      }
+
+      if (this.camera.position.x > 2350) {
+        new TWEEN.Tween(this.camera.position)
+        .to({ x: 2350 }, 3000)
+        .easing(TWEEN.Easing.Quadratic.Out)
+        .start();
+      }
+      
+      this.camera.position.x += (this.mouseX * 100 - this.camera.position.x) * 0.005;
     },
     onWindowResize: function () {
       this.windowHalfX = window.innerWidth / 2;
@@ -224,6 +421,7 @@ export default {
   mounted () {
     this.roadmapScene();
     this.animate();
+    document.getElementById('app').addEventListener('wheel', this.wheelScroll, false);
     document.addEventListener('mouseup', this.onPointerUp, false);
     document.addEventListener('mousedown', this.onPointerDown, false);
     window.addEventListener('resize', this.onWindowResize, false);

b3cec7864f8b68690f82658820fe202cd6ab8bfe
David Tavadze <tavadzed@gmail.com>
2021-07-26 11:32:41 +0400

    added movement to roadmap

--- src/views/Roadmap.vue
+++ src/views/Roadmap.vue
@@ -6,7 +6,7 @@
 
 <script>
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+const TWEEN = require('@tweenjs/tween.js');
 
 export default {
   name: 'Home',
@@ -27,7 +27,11 @@ export default {
       windowHalfY: window.innerHeight / 2,
       positions: null,
       controls: null,
-      uniforms: null
+      uniforms: null,
+      isPointerDown: false,
+      direction: "",
+      oldY: 0,
+      count: 0
     }
   },
   methods: {
@@ -41,7 +45,7 @@ export default {
 
       this.scene = new THREE.Scene();
 
-      this.roadmapGeo = new THREE.PlaneBufferGeometry(2000*1.5, 100*1.5, 2000, 100);
+      this.roadmapGeo = new THREE.PlaneBufferGeometry(2000*1.5, 80*1.5, 2000, 80);
       const loader = new THREE.TextureLoader();
       const texture = loader.load(require("../assets/wave_color.png"));
 
@@ -104,35 +108,79 @@ export default {
       this.scene.add(this.roadmapMesh);
       this.renderer = new THREE.WebGLRenderer();
       this.renderer.setSize(window.innerWidth, window.innerHeight);
-      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-      this.controls.update();
+      this.moveRoadmapToStart();
 
       container.appendChild(this.renderer.domElement);
     },
+    moveRoadmapToStart: function () {
+      new TWEEN.Tween(this.roadmapMesh.position)
+      .to({ x: 1100 }, 3000)
+      .easing(TWEEN.Easing.Quadratic.Out)
+      .start();
+    },
     animate: function () {
+      let center = new THREE.Vector3(0,0,0);
+      const theTime = performance.now() * .001;
       var pos = this.roadmapGeo.attributes.position;
-      var vec3 = new THREE.Vector3(); // for re-use
+      var vec3 = new THREE.Vector3(); 
       var magnitude = 5;
       var waveSize = 12;
+      var rotationAngle = 0.07;
 
-      const theTime = performance.now() * .001;
+      if (this.isPointerDown) {
+        this.camera.position.x += (this.mouseX * 4 - this.camera.position.x) * 0.005;
+        if (this.camera.position.x < -300 || this.camera.position.x > 2500) {
+          this.isPointerDown = false;
+        }
+
+        if (this.camera.position.x < -200) {
+          new TWEEN.Tween(this.camera.position)
+          .to({ x: -200 }, 3000)
+          .easing(TWEEN.Easing.Quadratic.Out)
+          .start();
+        }
+
+        if (this.camera.position.x > 2350) {
+          new TWEEN.Tween(this.camera.position)
+          .to({ x: 2350 }, 3000)
+          .easing(TWEEN.Easing.Quadratic.Out)
+          .start();
+        }
+
+        if (this.direction === "up" && this.roadmapMesh.rotation.x < 2.1) {
+          this.roadmapMesh.rotateX(+rotationAngle);
+        } 
+
+        if (this.direction === "down" && this.roadmapMesh.rotation.x > 1.8) {
+          this.roadmapMesh.rotateX(-rotationAngle);
+        }
+      }
+
       for (var i = 0, l = pos.count; i < l; i++) {
         vec3.fromBufferAttribute(pos, i);
+        vec3.sub(center);
         var z = Math.sin(vec3.length() /- waveSize + (theTime)) * magnitude;
-        var y = Math.sin(vec3.length() /- waveSize + (theTime)) * magnitude;
         pos.setZ(i, z);
       }
 
       pos.needsUpdate = true;
+
+      /*let iSin = 0;
+      for (let ix = 0, l = pos.count; ix < l; ix++) {
+        for (let iy = 0, h = pos.count; iy < h; iy++) {
+          pos[iSin + 1] = (Math.sin((ix + this.count) * 0.1) * 150) + (Math.sin((iy + this.count) * 0.3) * 150);
+
+          iSin += 3;
+        }
+      }
+      this.roadmapMesh.geometry.attributes.position.needsUpdate = true;
+      this.count += 0.1;*/
+
       requestAnimationFrame(this.animate);
-      this.controls.update();
+      TWEEN.update();
       this.render();
     },
     render: function () {
-      //this.camera.position.x += (this.mouseX - this.camera.position.x) * .05;
-      //this.camera.position.y += (-this.mouseY - this.camera.position.y) * .05;
-      this.camera.lookAt(this.scene.position);
-
       this.renderer.setPixelRatio(window.devicePixelRatio);
       this.renderer.render(this.scene, this.camera);
     },
@@ -145,8 +193,29 @@ export default {
       this.renderer.setSize(window.innerWidth, window.innerHeight);
       this.render();
     },
+    onPointerDown: function (event) {
+      if (event.isPrimary === false) return;
+      this.isPointerDown = true;
+    },
+    onPointerUp: function () {
+      if (event.isPrimary === false) return;
+      this.isPointerDown = false;
+
+      new TWEEN.Tween(this.roadmapMesh.rotation)
+      .to({ x: 2 }, 3000)
+      .easing(TWEEN.Easing.Quadratic.Out)
+      .start();
+    },
     onPointerMove: function (event) {
       if (event.isPrimary === false) return;
+
+      if (event.pageY < this.oldY) {
+        this.direction = "up";
+      } else if (event.pageY > this.oldY) {
+        this.direction = "down";
+      }
+
+      this.oldY = event.pageY;
 
       this.mouseX = event.clientX - this.windowHalfX;
       this.mouseY = event.clientY - this.windowHalfY;
@@ -155,6 +224,8 @@ export default {
   mounted () {
     this.roadmapScene();
     this.animate();
+    document.addEventListener('mouseup', this.onPointerUp, false);
+    document.addEventListener('mousedown', this.onPointerDown, false);
     window.addEventListener('resize', this.onWindowResize, false);
     window.addEventListener('pointermove', this.onPointerMove);
   }
